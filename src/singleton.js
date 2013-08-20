6e3eac4e4d0e6c3b39708f32dd91b147de3a5932
Bob Gray <gray.bob98@gmail.com>
2013-08-20 14:53:32 -0500

    Adding singleton module. An abstract singleton class

---
+++ src/singleton.js
@@ -0,0 +1,32 @@
+define(
+	[
+		"./meta",
+		"./class",
+		"./base"
+	],
+	function (meta, Class, Base) {
+		var Singleton = Class(function () {
+			this.invoke("addInstance");
+		});
+
+		Singleton.extends(Base);
+
+		function addInstance () {
+			var constructor = this.constructor;
+			if (!constructor.instances) {
+				constructor.instances = [];
+			}
+			constructor.instances.unshift(this);
+		}
+
+		Singleton.method(meta({
+			"entity": "method",
+			"for": "Singleton",
+			"name": "getInstance",
+			"description": "Useful for getting the singleton instance from class constructor.",
+			"implementation": function () {
+				return this.instances && this.instances[0];
+			}
+		}));
+	}
+);
