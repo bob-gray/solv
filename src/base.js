9b2d79461e3ffe25ad5d69c3985732d2dc994668
Bob Gray <gray.bob98@gmail.com>
2013-08-21 10:39:27 -0500

    cleaning up meta and tweaking method implementations

--- src/base.js
+++ src/base.js
@@ -15,109 +15,134 @@ define(
 			})
 		);
 
+		Base.method(
+			meta({
+				"name": "superConstructor",
+				"description": "Calls this._super.constructor with this as the context.",
+				"arguments": [{
+					"type": "any",
+					"required": false,
+					"repeating": true
+				}],
+				"implementation": superConstructor
+			})
+		);
 
+		Base.method(
+			meta({
+				"name": "superConstructorApply",
+				"description": "Applies this._super.constructor with this as the context.",
+				"signature": "arguments|array",
+				"arguments": [{
+					"type": "arguments|array",
+					"name": "args",
+					"required": false
+				}],
+				"implementation": superConstructorApply
+			})
+		);
 
-		Base.method(meta({
-			"name": "superConstructor",
-			"description": "Calls this._super.constructor with this as the context.",
-			"arguments": [{
-				"type": "any",
-				"required": false,
-				"repeating": true
-			}],
-			"implementation": superConstructor
-		}));
+		Base.method(
+			meta({
+				"name": "superInvoke",
+				"description": "Invokes a super method with this as the context.",
+				"arguments": [{
+					"type": "string",
+					"name": "method",
+					"required": true
+				}, {
+					"type": "any",
+					"required": false,
+					"repeating": true
+				}],
+				"implementation": superInvoke,
+				"returns": "any"
+			})
+		);
 
-		Base.method(meta({
-			"name": "superConstructorApply",
-			"description": "Applies this._super.constructor with this as the context.",
-			"signature": "arguments|array",
-			"arguments": [{
-				"type": "arguments|array",
-				"name": "args",
-				"required": false
-			}],
-			"implementation": superConstructorApply
-		}));
+		Base.method
+			meta({
+				"name": "superApply",
+				"description": "Invokes a super method with this as the context; applying args.",
+				"arguments": [{
+					"type": "arguments|array",
+					"name": "args",
+					"required": false
+				}],
+				"implementation": superApply,
+				"returns": "any"
+			})
+		);
 
-		Base.method(meta({
-			"name": "superInvoke",
-			"description": "Invokes a super method with this as the context.",
-			"arguments": [{
-				"type": "string",
-				"name": "method",
-				"required": true
-			}, {
-				"type": "any",
-				"required": false,
-				"repeating": true
-			}],
-			"implementation": superInvoke
-		}));
+		Base.method(
+			meta({
+				"name": "invoke",
+				"description": "Useful for invoking local functions as private methods.",
+				"arguments": [{
+					"type": "function",
+					"name": "fn"
+				}, {
+					"type": "any",
+					"required": false,
+					"repeating": true
+				}],
+				"implementation": invokeFunction,
+				"returns": "any"
+			})
+		);
 
-		Base.method(meta({
-			"name": "superApply",
-			"description": "Invokes a super method with this as the context; applying args.",
-			"arguments": [{
-				"type": "arguments|array",
-				"name": "args",
-				"required": false
-			}],
-			"implementation": superApply
-		}));
+		Base.method(
+			meta({
+				"name": "invoke",
+				"description": "Included for consistency with the proxy method api.",
+				"arguments": [{
+					"type": "string",
+					"name": "method"
+				}, {
+					"type": "any",
+					"required": false,
+					"repeating": true
+				}],
+				"implementation": invokeMethod,
+				"returns": "any"
+			})
+		);
 
-		Base.method(meta({
-			"name": "invoke",
-			"description": "Useful for invoking local functions as private methods",
-			"arguments": [{
-				"type": "function",
-				"name": "method"
-			}, {
-				"type": "any",
-				"required": false,
-				"repeating": true
-			}],
-			"implementation": invokeFunction
-		}));
+		Base.method(
+			meta({
+				"name": "proxy",
+				"description": "Creates a function bound to this; useful for attaching event listeners.",
+				"arguments": [{
+					"type": "function",
+					"name": "fn",
+					"description": "Any function in scope. Doesn't need to be a public method."
+				}, {
+					"type": "any",
+					"required": false,
+					"repeating": true
+				}],
+				"implementation": proxyFunction,
+				"returns": "function"
+			})
+		);
 
-		Base.method(meta({
-			"name": "invoke",
-			"arguments": [{
-				"type": "string",
-				"name": "method"
-			}, {
-				"type": "any",
-				"required": false,
-				"repeating": true
-			}],
-			"implementation": invokeMethod
-		}));
-
-		Base.method(meta({
-			"name": "proxy",
-			"arguments": [{
-				"type": "function",
-				"name": "method"
-			}, {
-				"type": "any",
-				"required": false,
-				"repeating": true
-			}],
-			"implementation": proxyFunction
-		}));
-
-		Base.method(meta({
-			"name": "proxy",
-			"arguments": [{
-				"type": "function",
-				"name": "method"
-			}, {
-				"type": "any",
-				"required": false,
-				"repeating": true
-			}],
-			"implementation": proxyMethod
-		}));
+		Base.method(
+			meta({
+				"name": "proxy",
+				"description": "Creates a function bound to this; useful for attaching event listeners.",
+				"arguments": [{
+					"type": "string",
+					"name": "method",
+					"description": "The name of the method to bind."
+				}, {
+					"type": "any",
+					"required": false,
+					"repeating": true
+				}],
+				"implementation": proxyMethod,
+				"returns": "function"
+			})
+		);
 
 		Base.prototype._super = {};
 
@@ -131,7 +156,7 @@ define(
 
 		function superInvoke (method) {
 			var args = Array.fromArguments(arguments).slice(1);
-			return this.superInvoke(method, args);
+			return this.superApply(method, args);
 		}
 
 		function superApply (method, args) {
@@ -153,9 +178,9 @@ define(
 
 		function proxyMethod (method) {
 			var args = Array.fromArguments(arguments).slice(1),
-				method = this[method];
+				fn = this[method];
 			args.splice(0, 1, this);
-			return method.bind.apply(method, args);
+			return fn.bind.apply(fn, args);
 		}
 
 		function proxyFunction (fn) {
