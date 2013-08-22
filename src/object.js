32c3701ed7e95e4f8a6ac7d0218581a7e05fbc0e
Bob Gray <gray.bob98@gmail.com>
2013-08-22 10:59:40 -0500

    adding Object shim

---
+++ src/object.js
@@ -0,0 +1,26 @@
+define(
+	[
+		"./meta"
+	],
+	function (meta) {
+		if (!Object.keys) {
+			Object.keys = function(object) {
+				var keys = [];
+				if (isValidObject(object)) {
+					throw TypeError("Object.keys called on non-object");
+				}
+				for (var key in object) {
+					if (object.hasOwnProperty(key)) {
+						keys.push(key);
+					}
+				}
+				return keys;
+			}
+		}
+
+		function isValidObject (object) {
+			var type = typeof object;
+			return type != "object" && type != "function" || object == null;
+		}
+	}
+);
