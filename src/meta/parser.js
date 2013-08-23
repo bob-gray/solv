90a8ebdb0702c5fef5cbedcf60d08010cec48649
Bob Gray <gray.bob98@gmail.com>
2013-08-22 23:32:09 -0500

    overhauling meta/parser

--- src/meta/parser.js
+++ src/meta/parser.js
@@ -1,58 +1,58 @@
 define(
 	[
 		"../meta",
+		"../class",
+		"../abstract/base",
 		"../shim/array"
 	],
-	function (meta) {
+	function (meta, Class, Base) {
 		"use strict";
 
-		function Application (root) {
-			if (!(this instanceof Application)) {
-				return new Application();
+		var Parser = Class(
+			meta({
+				"name": "Parser",
+				"extends": Base,
+				"arguments": [{
+					"name"; "root",
+					"type": "string"
+				}]
+			}),
+			function (root) {
+				this.root = root;
 			}
+		);
 
-			this.root = root;
+		Parser.regex = /\bmeta\s*\(((?:[^)"]*|".*")*)\)/g;
+
+		Parser.method(
+			meta({
+				"name": "getMeta",
+				"arguments": [{
+					"name": "success",
+					"type": "function"
+				}, {
+					"name": "fail",
+					"type": "function"
+				}]
+			}),
+			function (success, fail) {
+				ICEA.ajax({
+					url: this.root,
+					returnFormat: "plain",
+					error: fail,
+					callback: this.proxy(parse, success)
+				});
+			}
+		);
+
+		function parse (success, file) {
+			var blocks = [];
+			file.replace(findBlocks, function (match, block) {
+				blocks.push(JSON.parse(block));
+			});
+			return blocks;
 		}
 
-		Application.prototype.parseDocBlocks = function (blocks) {
-			var docs = {};
-
-			blocks.forEach(function (block) {
-				docs[block.name] = block;
-			});
-
-			return docs;
-		};
-
-		Application.prototype.getDocBlocks = (function () {
-			var findBlocks = /\bmeta\s*\(((?:[^)"]*|".*")*)\)/g;
-
-			return function (file) {
-				var blocks = [];
-
-				file.replace(findBlocks, function (match, block) {
-					blocks.push(JSON.parse(block));
-				});
-
-				return blocks;
-			};
-		}());
-
-		Application.prototype.getDocumentation = function (success, fail) {
-			var app = this;
-
-			ICEA.ajax({
-				url: this.root,
-				returnFormat: "plain",
-				error: fail,
-				callback: function (file) {
-					var blocks = app.getDocBlocks(file),
-						docs = app.parseDocBlocks(blocks);
-					success(docs);
-				}
-			});
-		};
-
-		return Application;
+		return Parser;
 	}
 );
