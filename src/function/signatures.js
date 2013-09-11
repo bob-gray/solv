712c6ec1604b6c45c36ec5d8bd49dbdcfdc4e960
Bob Gray <gray.bob98@gmail.com>
2013-09-11 10:24:59 -0500

    refactoring signature component regex and replacement text and adding return signature compiler

--- src/function/signatures.js
+++ src/function/signatures.js
@@ -16,7 +16,7 @@ define(
 			"name": "Invocation Signature",
 			"description": "Specification for defining the calling signature of a function.",
 			"overview":	"Function signatures in this module are special comma delimited lists of signature components that describe the argument types expected by a function. The length of signature components doesn't necessarily match the length of a matching arguments collection because arguments can be specified as optional or repeating. Signatures are composed of one or more comma delimited components. Components are composed of one or more pipe delimited types each optional prefix with an exclamation point. Each component may optional be suffixed with a meta character indicating quantity. All white space is ignored and argument types are assumed to be lower case single word identifiers listed below.",
-			"types": "string, number, boolean, array, object, date, regexp, function, undefined, null or *",
+			"types": "string, number, boolean, array, object, date, regexp, function, undefined, null or any",
 			"examples": {
 				"boolean?, string|number, function": "Matches (boolean, number, function) and (boolean, string, function) and (string,function) and (number,function). Does not match (boolean, function) or (boolean, number)",
 				"string, object, any, !null*": "Matches (string, object, object) and (string, object, number, number) and (string, object, null). Does not match (string, object, object, null)."
@@ -43,11 +43,28 @@ define(
 			"arguments": [{
 				"name": "signature",
 				"type": "string",
-				"description": "A comma delimited list that describes the argument types to be passed to a function. Can include \"?*+|!\". See function signature."
+				"description": "A comma delimited list that describes the argument types to be passed to a function. Can include '?*+|!'. See function signature."
 			}],
 			"return": {
 				"type": "regexp",
 				"description": "Can be used for testing against an arguments signature"
+			}
+		});
+
+		meta({
+			"entity": "method",
+			"for": "Function",
+			"name": "compileReturnSignature",
+			"static": true,
+			"description": "Transform a function return signature into regular expression capable of testing a function's return value type.",
+			"arguments": [{
+				"name": "signature",
+				"type": "string",
+				"description": "A pipe delimited list of possible return types. Can include '!'. See function signature."
+			}],
+			"return": {
+				"type": "regexp",
+				"description": "Can be used for testing against the type of a function's return value"
 			}
 		});
 
@@ -87,24 +104,53 @@ define(
 			}
 		});
 
-		var whiteSpace = /\s+/g,
-			noneOneMany = /,?([^,]+)([?*+])/g,
-			or = /((?:[^,?|()]+\|)+[^,?|()]+)/g,
-			any = /\bany\b/g,
-			not = /!([^,|]+)/g;
-
 		Function.compileImplementationSignature = function (signature) {
-			var regexSrc = createSignatureRegexSrc(signature);
+			var regexSrc = createImplementationSignatureSrc(signature);
 			return new RegExp(regexSrc);
 		};
 
-		function createSignatureRegexSrc (signature) {
+		Function.compileReturnSignature = function (signature) {
+			var regexSrc = createReturnSignatureSrc(signature);
+			return new RegExp(regexSrc);
+		};
+
+		var whiteSpace = {
+				matcher: /\s+/g,
+				replacement: ""
+			},
+			noneOneMany = {
+				matcher: /,?([^,]+)([?*+])/g,
+				replacement: "(?:,?$1)$2"
+			},
+			or = {
+				matcher: /((?:[^,?|()]+\|)+[^,?|()]+)/g,
+				replacement: "(?:$1)"
+			},
+			not = {
+				matcher: /!([^,|]+)/g,
+				replacement: "\\b(?!$1)[^,]+"
+			},
+			any = {
+				matcher: /\bany\b/g,
+				replacement: "[^,]+"
+			};
+
+		function createImplementationSignatureSrc (signature) {
 			return "^"+
-				signature.replace(whiteSpace, "")
-					.replace(noneOneMany, "(?:,?$1)$2")
-					.replace(or, "(?:$1)")
-					.replace(not, "\\b(?!$1)[^,]+")
-					.replace(any, "[^,]+")
+				signature.replace(whiteSpace.matcher, whiteSpace.replacement)
+					.replace(noneOneMany.matcher, noneOneMany.replacement)
+					.replace(or.matcher, or.replacement)
+					.replace(not.matcher, not.replacement)
+					.replace(any.matcher, any.replacement)
+				+"$";
+		}
+
+		function createReturnSignatureSrc (signature) {
+			return "^"+
+				signature.replace(whiteSpace.matcher, whiteSpace.replacement)
+					.replace(or.matcher, or.replacement)
+					.replace(not.matcher, not.replacement)
+					.replace(any.matcher, any.replacement)
 				+"$";
 		}
 
