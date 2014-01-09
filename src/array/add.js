35f41c1bfa7b3b5be2a7c58a581a9defb1f62ab3
Bob Gray <gray.bob98@gmail.com>
2014-01-09 12:09:47 -0500

    adding array.add method

---
+++ src/array/add.js
@@ -0,0 +1,45 @@
+if (typeof define !== "function") {
+	var define = require("amdefine")(module);
+}
+
+define(function (require) {
+	"use strict";
+
+	require("../class/method");
+	require("../shim/array");
+
+	var meta = require("../meta");
+
+	meta({
+		"name": "Array",
+		"type": "class",
+		"global": true
+	});
+	
+	Array.method(
+		meta({
+			"name": "add",
+			"shim": true,
+			"descriptions": "Push an item onto the array only if it isn't already found in the array",
+			"arguments": [{
+				"name": "item",
+				"type": "any"
+			}]
+		}),
+		add
+	);
+	
+	function add (item) {
+		var index = this.indexOf(item);
+		
+		if (isNotFound(index)) {
+			this.push(item);
+		}
+	}
+	
+	function isNotFound (index) {
+		return index === -1;
+	}
+	
+	return add;
+});
