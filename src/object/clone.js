97f697780c246af832228f9aa8203efc5307adb7
Bob Gray <gray.bob98@gmail.com>
2014-05-22 16:39:15 -0500

    replacing object/copy.deep with object/clone - a more robust circular reference safe deep copy

---
+++ src/object/clone.js
@@ -0,0 +1,89 @@
+if (typeof define !== "function") {
+	var define = require("amdefine")(module);
+}
+
+define(function (require) {
+	"use strict";
+
+	require("../shim/object");
+	require("../shim/array");
+	require("../class/shim");
+
+	var meta = require("../meta"),
+		type = require("../type"),
+		utils = require("./utils"),
+		originals,
+		copies;
+
+	meta({
+		"name": "Object",
+		"type": "class",
+		"global": true
+	});
+
+	meta({
+		"name": "clone",
+		"static": true,
+		"shim": true,
+		"description": "Creates a deep copy of all own properties that are types object, array, number, string, boolean, null, undefined. Circular references are supported.",
+		"arguments": [{
+			"name": "source",
+			"type": "object|array"
+		}],
+		"returns": "object|array"
+	});
+
+	Object.shimStatic(clone);
+
+	function clone (source) {
+		setup();
+		var target = getTargetValue(source);
+		teardown();
+
+		return target;
+	}
+
+	function setup () {
+		originals = [];
+		copies = [];
+	}
+
+	function teardown () {
+		originals = null;
+		copies = null;
+	}
+
+	function assign (target, name) {
+		var value = this[name],
+			valueType = type.of(value),
+			targetValue;
+
+		if (utils.complex.test(valueType)) {
+			target[name] = getTargetValue(value);
+
+		} else if (utils.simple.test(valueType)) {
+			target[name] = value;
+		}
+
+		return target;
+	}
+
+	function getTargetValue (value) {
+		var index = originals.indexOf(value),
+			targetValue;
+
+		if (index > -1) {
+			targetValue = copies[index];
+
+		} else {
+			targetValue = utils.getEmpty(type.of(value));
+			originals.push(value);
+			copies.push(targetValue);
+			targetValue = utils.copy(assign, targetValue, value);
+		}
+
+		return targetValue;
+	}
+
+	return clone;
+});
---
+++ src/object/utils.js
@@ -0,0 +1,46 @@
+if (typeof define !== "function") {
+	var define = require("amdefine")(module);
+}
+
+define(function (require) {
+	"use strict";
+
+	require("../shim/object");
+	require("../shim/array");
+	require("../shim/function");
+
+	function copy (assign, target, source) {
+		var properties = ownNonNullProperties(source);
+
+		return properties.reduce(assign.bind(source), target);
+	}
+
+	function ownNonNullProperties (source) {
+		return Object.keys(source).filter(isDefined, source);
+	}
+
+	function isDefined (name) {
+		/* jshint eqnull:true */
+		return this[name] != null;
+	}
+
+	function getEmpty (type) {
+		var empty;
+
+		if ("object" === type) {
+			empty = {};
+
+		} else if ("array" === type) {
+			empty = [];
+		}
+
+		return empty;
+	}
+
+	return {
+		simple: /string|number|boolean|null/,
+		complex: /object|array/,
+		copy: copy,
+		getEmpty: getEmpty
+	};
+});
