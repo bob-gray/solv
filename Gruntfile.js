215bd6229a0c422fbeba91f4d6d674df614d9d7b
Bob Gray <gray.bob98@gmail.com>
2014-06-06 23:19:47 -0500

    tweaking build and test setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,109 +1,51 @@
+"use strict";
+
 module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
 		watch: {
-			files: [
-				"src/**/*.js",
-				"tests/spec/**/*.js"
-			],
-			tasks: [
-				//"lint",
-				"karma:watch:run"
-			]
+			all: {
+				files: [
+					"src/**/*.js",
+					"tests/spec/**/*.js"
+				],
+				tasks: [
+					"lint",
+					"karma:watch:run"
+				]
+			}
 		},
 		jshint: {
 			options: {
-				strict: true,
-				curly: true,
-				eqeqeq: true,
-				forin: true,
-				immed: true,
-				latedef: "nofunc",
-				newcap: true,
-				noarg: true,
-				noempty: true,
-				nonew: true,
-				plusplus: true,
-				quotmark: true,
-				undef: true,
-				validthis: true,
-				maxparams: 3,
-				maxdepth: 2,
-				//maxstatements: 5,
-				maxcomplexity: 5,
-				globals: {
-					define: true, // amdefine
-					require: false,
-					module: false,
-					setTimeout: false
-				}
+				jshintrc: ".jshintrc"
 			},
-			src: [
-				"src/**/*.js"
-			],
+			lib: {
+				src: [
+					"src/**/*.js"
+				]
+			},
 			tests: {
-				files: {
-					src: [
-						"tests/runner.js",
-						"tests/karma/runner.js",
-						"tests/spec/**/*.js"
-					]
-				},
-				options: {
-					globals: {
-						require: false,
-						define: false,
-						jasmine: false,
-						describe: false,
-						beforeEach: false,
-						afterEach: false,
-						it: false,
-						expect: false,
-						__karma__: false
-					}
-				}
+				src: [
+					"tests/runner.js",
+					"tests/karma/runner.js",
+					"tests/spec/**/*.js"
+				]
 			}
 		},
 		karma: {
 			options: {
-				files: [{
-					pattern: "src/**/*.js",
-					included: false
-				}, {
-					pattern: "tests/spec/**/*-test.js",
-					included: false
-				}, "tests/karma/runner.js"],
-				frameworks: [
-					"jasmine",
-					"requirejs"
-				]
+				configFile: "karma.conf.js"
 			},
 			watch: {
-				browsers: [
-					"PhantomJS"
-				],
-				hostname: process.env.IP,
-				port: process.env.PORT,
-				runnerPort: 0,
+				singleRun: false,
 				background: true
 			},
 			phantom: {
 				browsers: [
 					"PhantomJS"
-				],
-				hostname: process.env.IP,
-				port: process.env.PORT,
-				runnerPort: 0,
-				singleRun: true
+				]
 			},
 			coverage: {
-				browsers: [
-					//"Chrome",
-					//"Firefox",
-					//"IE",
-					//"Safari",
-					"PhantomJS"
-                ],
 				preprocessors: {
 					"src/**/*.js": [
 						"coverage"
@@ -115,12 +57,8 @@ module.exports = function (grunt) {
 				],
 				coverageReporter: {
 					type: "html",
-					dir: "tests/coverage/"
-				},
-				hostname: process.env.IP,
-				port: process.env.PORT,
-				runnerPort: 0,
-				singleRun: true
+					dir: "coverage"
+				}
 			}
 		},
 		"api-meta": {
@@ -153,7 +91,7 @@ module.exports = function (grunt) {
 		"karma:coverage"
 	]);
 	
-	grunt.registerTask("startWatch", [
+	grunt.registerTask("start-watch", [
 		"karma:watch:start",
 		"watch"
 	]);
---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = function (config) {
+	config.set({
+		files: [{
+			pattern: "src/**/*.js",
+			included: false
+		}, {
+			pattern: "tests/spec/**/*-test.js",
+			included: false
+		}, {
+			pattern: "tests/karma-runner.js",
+			included: true
+		}],
+		frameworks: [
+			"jasmine",
+			"requirejs"
+		],
+		browsers: [
+			"PhantomJS",
+			"Chrome",
+			"Firefox",
+			"Safari",
+			"IE"
+        ],
+		singleRun: true
+	});
+};
