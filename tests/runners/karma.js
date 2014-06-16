14a6e3803baea481addd2930f357074b26fe36af
Bob Gray <gray.bob98@gmail.com>
2014-06-15 22:57:42 -0500

    making runner ie8 compatible

--- tests/runners/karma.js
+++ tests/runners/karma.js
@@ -1,13 +1,9 @@
 (function loadTestSuiteModules () {
 	"use strict";
 
-	var karmaFiles = Object.keys(__karma__.files),
+	var karmaFiles = keys(__karma__.files),
 		testModulePattern = /-test\.js$/,
-		testModules = karmaFiles.filter(isTestModule);
-
-	function isTestModule (file) {
-		return testModulePattern.test(file);
-	}
+		testModules = filter(karmaFiles, isTestModule);
 
 	require.config({
 		baseUrl: "/base",
@@ -15,6 +11,50 @@
 			"solv": "src"
 		}
 	});
-	
+
 	require(testModules, __karma__.start);
+
+	function keys (object) {
+		if (Object.keys) {
+			return Object.keys(object);
+		}
+
+		var keys = [],
+			key;
+
+		for (key in object) {
+
+			if (object.hasOwnProperty(key)) {
+				keys.push(key);
+			}
+		}
+
+		return keys;
+	}
+
+	function filter (array, fn) {
+		if (array.filter) {
+			return array.filter(fn);
+		}
+
+		var i = 0,
+			length = array.length,
+			filtered = [],
+			item;
+
+		for (; i < length; i += 1) {
+
+			item = array[i];
+
+			if (fn(item)) {
+				filtered.push(item);
+			}
+		}
+
+		return filtered;
+	}
+
+	function isTestModule (file) {
+		return testModulePattern.test(file);
+	}
 }());
