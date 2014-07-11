b1c731305d28d6bb3c4f8596fbf029d9003b91bb
Bob Gray <gray.bob98@gmail.com>
2014-07-10 23:29:42 -0500

    finishing array/add-test and array-contains-test and updating date tests

--- tests/spec/array/contains-test.js
+++ tests/spec/array/contains-test.js
@@ -2,6 +2,29 @@ define(["solv/array/contains"], function () {
 	"use strict";
 
 	describe("array.contains", function () {
-		// TODO: write array.contains suite
+		var array,
+			string,
+			number,
+			object;
+
+		beforeEach(function () {
+			string = "string";
+			number = 100;
+			object = {};
+			array = [string, object, string];
+		});
+
+		it("should return true for values found in the array", function () {
+			expect(array.contains(string)).toBe(true);
+		});
+
+		it("should return false for values not found in the array", function () {
+			expect(array.contains(number)).toBe(false);
+		});
+
+		it("should work with objects", function () {
+			expect(array.contains(object)).toBe(true);
+			expect(array.contains({})).toBe(false);
+		});
 	});
 });
