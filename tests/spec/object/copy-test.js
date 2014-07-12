ca0cf2bf4468877b04d136bcf0f60c81fd8d4a33
Bob Gray <gray.bob98@gmail.com>
2014-07-11 23:26:47 -0500

    finishing tests for Object.copy and Object.isEmpty

--- tests/spec/object/copy-test.js
+++ tests/spec/object/copy-test.js
@@ -2,6 +2,41 @@ define(["solv/object/copy"], function () {
 	"use strict";
 
 	describe("Object.copy", function () {
-		// TODO: write copy suite
+		var original,
+			copy;
+
+		function Thing () {}
+		Thing.prototype.inherited = "property";
+
+		beforeEach(function () {
+			original = new Thing();
+			original.own = "property";
+			original.nested = {
+				deep: true
+			};
+			original.nil = null;
+			original.not = undefined;
+			copy = Object.copy(original);
+		});
+
+		it("should make a copy", function () {
+			expect(copy.own).toBe(original.own);
+		});
+
+		it("should not make a deep copy", function () {
+			expect(copy.nested).toBe(original.nested);
+		});
+
+		it("should not copy inherited properties", function () {
+			expect("inherited" in copy).toBe(false);
+		});
+
+		it("should not copy null properties", function () {
+			expect("nil" in copy).toBe(false);
+		});
+
+		it("should not copy undefined properties", function () {
+			expect("not" in copy).toBe(false);
+		});
 	});
 });
--- tests/spec/object/is-empty-test.js
+++ tests/spec/object/is-empty-test.js
@@ -2,6 +2,12 @@ define(["solv/object/is-empty"], function () {
 	"use strict";
 
 	describe("Object.isEmpty", function () {
-		// TODO: write isEmpty suite
+		it("should return true for an empty object", function () {
+			expect(Object.isEmpty({})).toBe(true);
+		});
+
+		it("should return false for an object with properties", function () {
+			expect(Object.isEmpty({empty: false})).toBe(false);
+		});
 	});
 });
