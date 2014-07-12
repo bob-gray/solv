50fee8270e686d50eed62399595d04bd4e98c8c7
Bob Gray <gray.bob98@gmail.com>
2014-07-11 22:30:33 -0500

    refactor, fixes and finishing test for Object.validate

--- src/object/validate.js
+++ src/object/validate.js
@@ -6,7 +6,7 @@ if (typeof define !== "function") {
 define(function (require) {
 	"use strict";
 
-	require("../class/shim");
+	require("../class/method");
 	require("./for-each");
 
 	var meta = require("../meta"),
@@ -14,82 +14,91 @@ define(function (require) {
 		signatures = require("../function/signatures"),
 		InvalidPropertyType = require("../error/invalid-property-type");
 
-	meta({
+	/*meta({
 		"name": "Object",
 		"type": "class",
 		"global": true
-	});
+	})*/
 
-	meta({
-		"name": "validate",
-		"static": true,
-		"shim": true,
-		"throws": "InvalidPropertyType",
-		"description": "Tests an object against meta data to ensure all required properties are present and all properties are of the correct type",
-		"arguments": [{
-			"name": "object",
-			"type": "object"
-		}, {
-			"name": "properties",
-			"type": "object",
-			"description": "Key names should match object. Members can be a string representing the type(s) or an objects with properties 'type' and 'required'. Both are optional. Default is {type:'any', required:true}"
-		}]
-	});
+	Object.method(
+		meta({
+			"name": "validate",
+			"static": true,
+			"shim": true,
+			"throws": "InvalidPropertyType",
+			"description": "Tests an object against meta data to ensure all required properties are present and all properties are of the correct type",
+			"arguments": [{
+				"name": "object",
+				"type": "object"
+			}, {
+				"name": "properties",
+				"type": "object",
+				"description": "Key names should match object. Members can be a string representing the type(s) or an objects with properties 'type' and 'required'. Both are optional. Default is {type:'any', required:true}"
+			}]
+		}),
+		validate
+	);
 
-	Object.shimStatic("validate", validate);
-	
 	function validate (object, properties) {
 		Object.forEach(properties, testProperty, object);
 	}
-	
+
 	function testProperty (property, key) {
 		var value = this[key],
-			expectedType = getExpectedType(property),
+			signature = getSignature(property),
 			actualType = type.of(value);
 
-		if (isInvalid(expectedType, actualType)) {
+		if (isInvalid(signature, actualType)) {
 			throw new InvalidPropertyType({
 				name: key,
-				expected: expectedType,
+				expected: signature,
 				actual: actualType
 			});
 		}
 	}
-	
+
+	function getSignature (property) {
+		var signature = getExpectedType(property),
+			required = isRequired(property);
+
+		if (!required && !signature) {
+			signature = "any";
+		} else if (!required) {
+			signature += "|undefined";
+		} else if (!signature) {
+			signature = "!undefined";
+		}
+
+		return signature;
+	}
+
 	function getExpectedType (property) {
 		var expectedType;
-		
+
 		if (type.is("string", property)) {
 			expectedType = property;
-		
 		} else if (type.is("string", property.type)) {
 			expectedType = property.type;
-		
-		} else if (isRequired(property)) {
-			expectedType = "!undefined|null";
-		
-		} else {
-			expectedType = "any";
 		}
-		
+
 		return expectedType;
 	}
-	
-	function isInvalid (expectedType, actualType) {
-		var tester = signatures.compileReturnSignature(expectedType);
-		
-		return tester.test(actualType);
+
+	function isInvalid (signature, actualType) {
+		var tester = signatures.compileReturnSignature(signature);
+
+		return tester.test(actualType) === false;
 	}
-	
+
 	function isRequired (property) {
-		var required = true;
-		
-		if (type.is.not("undefined", property.requried)) {
+		var required;
+
+		if (type.is("boolean", property.required)) {
 			required = property.required;
+		} else {
+			required = true;
 		}
-		
+
 		return required;
 	}
-	
-	return validate;
 });
--- tests/spec/object/validate-test.js
+++ tests/spec/object/validate-test.js
@@ -2,6 +2,109 @@ define(["solv/object/validate"], function () {
 	"use strict";
 
 	describe("Object.validate", function () {
-		// TODO: write validate suite
+		var vehicle;
+
+		beforeEach(function () {
+			vehicle = {
+				make: "Ford",
+				model: "Thunderbird",
+				year: 1981,
+				rwd: true,
+				options: [
+					"ac",
+					"power steering"
+				]
+			};
+		});
+
+		it("should not error if object has all required properties", function () {
+			Object.validate(vehicle, {
+				make: "string",
+				model: "string",
+				year: "number"
+			});
+		});
+
+		it("should not error if object has extra properties", function () {
+			vehicle.damaged = true;
+
+			Object.validate(vehicle, {
+				make: "string",
+				model: "string",
+				year: "number"
+			});
+		});
+
+		it("should accept property description as object {type, required}", function () {
+			Object.validate(vehicle, {
+				make: {
+					type: "string",
+					required: true
+				},
+				options: {
+					type: "array",
+					required: false
+				}
+			});
+		});
+
+		it("should set required to false", function () {
+			Object.validate(vehicle, {
+				owner: {
+					type: "object",
+					required: false
+				}
+			});
+		});
+
+		it("should error if object is missing required properties", function () {
+			expect(missingProperties).toThrow();
+
+			function missingProperties () {
+				Object.validate(vehicle, {
+					owner: {
+						type: "string"
+					}
+				});
+			}
+		});
+
+		it("should default property required to true", function () {
+			expect(missingProperties).toThrow();
+
+			function missingProperties () {
+				Object.validate(vehicle, {
+					owner: "object"
+				});
+			}
+		});
+
+		it("should default property type to any", function () {
+			Object.validate(vehicle, {
+				make: {
+					required: true
+				}
+			});
+		});
+
+		it("should error if property is wrong type", function () {
+			expect(missingProperties).toThrow();
+
+			function missingProperties () {
+				Object.validate(vehicle, {
+					year: "string"
+				});
+			}
+		});
+
+		it("should error if optional property is wrong type", function () {
+			expect(missingProperties).toThrow();
+
+			function missingProperties () {
+				Object.validate(vehicle, {
+					options: "object"
+				});
+			}
+		});
 	});
 });
