4c4bd87b0a750bb3cd400fb5edfca1a8d9beb04a
Bob Gray <gray.bob98@gmail.com>
2014-12-30 00:04:20 -0600

    remove test of sparse array - it fails in ie 8 and it really isn't a neccessary test

--- tests/spec/array/sum-test.js
+++ tests/spec/array/sum-test.js
@@ -8,14 +8,6 @@ define(["solv/array/sum"], function () {
 			expect([10,100,1000].sum()).toBe(1110);
 		});
 
-		it("should work on sparse arrays", function () {
-			var array = [];
-
-			array[5] = 10;
-
-			expect(array.sum()).toBe(10);
-		});
-
 		it("should return zero if array is empty", function () {
 			expect([].sum()).toBe(0);
 		});
