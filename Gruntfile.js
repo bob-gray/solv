5177d56a0789cf2d3ad1d5782e9942004eefeb77
Bob Gray <gray.bob98@gmail.com>
2020-05-12 13:10:47 -0500

    bumping version to 1.1.0
    Allowing class init function to return an object and adding new object shims

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,19 +13,19 @@ function registerTasks (grunt) {
 		"complexity",
 		"test"
 	]);
-	
+
 	grunt.registerTask("lint", [
 		"jshint"
 	]);
-	
+
 	grunt.registerTask("test", [
 		"karma:local"
 	]);
-	
+
 	grunt.registerTask("coverage", [
 		"karma:coverage"
 	]);
-	
+
 	grunt.registerTask("start-watch", [
 		"karma:watch:start",
 		"watch"
